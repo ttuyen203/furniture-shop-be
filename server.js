3c53c71a8b561a8ab9b6f35880fbeae9913d0511
ttuyen <totuyencv@gmail.com>
2024-09-02 23:38:54 +0700

    [#1] - Setup backend

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(cors());
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch((err) => {
+    console.log("Connection failed", err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
